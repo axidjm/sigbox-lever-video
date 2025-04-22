433793e21fffce888654e6a6ce6c8d13b87c0b1f
David Moore <david@axitech.co.uk>
2025-04-22 14:42:44 +0100

    trivial delays in test

--- test-api.bat
+++ test-api.bat
@@ -7,10 +7,10 @@ delay 1
 curl -X GET localhost:%port/lever/13/R
 delay 10
 curl -X GET localhost:%port/lever/13/N
-delay 1
+delay 20
 
 
 curl -X GET localhost:%port/lever/14/R
-delay 10
+delay 20
 curl -X GET localhost:%port/lever/14/N
 delay 1
