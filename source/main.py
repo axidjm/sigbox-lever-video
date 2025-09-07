8b136fe0454b4e6fb08adc44acbba618d3591441
David Moore <david@axitech.co.uk>
2025-09-07 20:52:10 +0100

    Allow a video to be cut short by another lever action, but don't allow trains to 'stack up'

--- source/main.py
+++ source/main.py
@@ -7,18 +7,21 @@ import uvicorn
 import vlc
 from fastapi import FastAPI
 
-
-version = '1.0'
+version = "1.1"
 app = FastAPI()
 levers = []
-for x in range(0,17):
-    levers.append('N')
+player = None
+train_playing = None
+
+for x in range(0, 17):
+    levers.append("N")
+
 
 @app.get("/")
 async def root():
-    messages =  {"message": "sigbox-lever-video", "version": version}
-    for x in range(1,17):
-        if levers[x] != 'N':
+    messages = {"message": "sigbox-lever-video", "version": version}
+    for x in range(1, 17):
+        if levers[x] != "N":
             print(f"lever {x} is {levers[x]}")
             messages[str(x)] = levers[x]
     return messages
@@ -26,38 +29,41 @@ async def root():
 
 @app.put("/lever/{lever_id}/{state}")
 async def handle_lever(lever_id: int, state):
+    global train_playing
+    global player
+
     # await debug_sound(lever_id, state)
     levers[lever_id] = state
-    
+    print(f"Playing: {train_playing}")
+
     match lever_id:
-        case 1:
-            if state == "R":
-                time.sleep(8)
-                # Switch to Green wire: Stopping passenger
-                # Switch to Brown wire: Mineral train
-                # Switch central: No effects
-                if levers[15] == 'R':
-                    await play_video("Down-Stopping-Local.mp4")
-                elif levers[16] == 'R':
-                    await play_video("Down-Mineral.mp4")
-                
-        case 12:
+        case 1 | 13:
             if state == "N":
-                await play_video("Signal-12-replaced.mp4")
+                print(f"Distant lever {lever_id} replaced")
+                return
+
+            if train_playing:
+                print("Already playing a train")
+                return
+
+            train = get_train_type()
+            if not train:
+                return
+
+            train_playing = "Down" if lever_id == 1 else "Up"
+            print(f"{train_playing} {train} approaching")
+
+            time.sleep(8)
+            await play_video(f"{train_playing}-{train}.mp4", 30)
+
+        case 11 | 12:
+            if state == "N":
+                if not player:
+                    # Only play returning the signal to danger if nothing else is playing
+                    await play_video("Signal-12-replaced.mp4")
             else:
                 await play_video("Signal-12-cleared.mp4")
 
-        case 13:
-            if state == "R":
-                time.sleep(8)
-                # Switch to Green wire: Stopping passenger
-                # Switch to Brown wire: Mineral train
-                # Switch central: No effects
-                if levers[15] == 'R':
-                    await play_video("Up-Stopping-Local.mp4")
-                elif levers[16] == 'R':
-                    await play_video("Up-Mineral.mp4")
-                    
         case 4 | 8 | 10:
             if state == "N":
                 await play_video("Points and Ground Signal 2.mp4")
@@ -70,26 +76,60 @@ async def handle_lever(lever_id: int, state):
             else:
                 await play_video("2-Gates-closing.mp4")
 
+        case 15 | 16:
+            get_train_type()
+
+            if player:
+                player.release()
+                player = None
+                train_playing = None
+
         case _:
             pass
 
     return {"lever_id": lever_id, "state": state}
 
 
-async def play_video(filename: str):
+def get_train_type():
+    # Switch to Green wire: Stopping passenger
+    # Switch to Brown wire: Mineral train
+    # Switch central: No effects
+    if levers[15] == "R":
+        print("Stopping-Local train selected")
+        return "Stopping-Local"
+    elif levers[16] == "R":
+        print("Mineral train selected")
+        return "Mineral"
+    print("No train type selected")
+    return None
+
+
+async def play_video(filename: str, min_play_seconds=30):
+    global player
+    global train_playing
+    # Play at least {min_play_seconds}. Return then, or when the video has finished, whichever is soonest.
     pathname = f"c:/sigbox/videos/{filename}"
     print("playing " + pathname)
+
+    if player:
+        player.release()
+
     player = vlc.MediaPlayer(pathname)
     player.set_fullscreen(1)
     player.play()
 
+    end_time = time.time() + min_play_seconds
     # Wait for is_playing to register the fact that it is playing
     time.sleep(0.1)
-    while True:
+
+    while time.time() < end_time:
         # app.processEvents()
+        time.sleep(1)
 
         if not player.is_playing():
             player.release()
+            player = None
+            train_playing = None
             return
 
 
@@ -99,25 +139,26 @@ async def debug_sound(lever_id: int, state):
     # print("playing " + pathname)
     player = vlc.MediaPlayer(pathname)
     player.play()
-    
+
     time.sleep(0.1)
     while player.is_playing():
         time.sleep(0.01)
-    
+
     match state:
-        case 'N':
+        case "N":
             pathname = "c:/sigbox/debug sounds/Normal.m4a"
-            
-        case 'R':
+
+        case "R":
             pathname = "c:/sigbox/debug sounds/Reversed.m4a"
-            
+
         case _:
             return
-    
+
     # print("playing " + pathname)
     player = vlc.MediaPlayer(pathname)
     player.play()
-    
+
+
 async def play_sound(filename: str):
     pathname = f"c:/sigbox/sounds/{filename}"
     print("playing " + pathname)
